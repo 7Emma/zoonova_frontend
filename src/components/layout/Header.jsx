6328e1865c4335820442284e28cbb92e0d05543d
Emmanuel AGBOTOEDO <205164160+emmaagbo@users.noreply.github.com>
2025-12-05 18:17:45 +0100

    update

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -19,11 +19,18 @@ export default function Navbar() {
   // Gère le scroll quand le menu est ouvert
   useEffect(() => {
     if (isMenuOpen) {
+      const scrollbarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
       document.body.style.overflow = "hidden";
+      document.body.style.paddingRight = scrollbarWidth + "px";
     } else {
       document.body.style.overflow = "";
+      document.body.style.paddingRight = "";
     }
-    return () => (document.body.style.overflow = "");
+    return () => {
+      document.body.style.overflow = "";
+      document.body.style.paddingRight = "";
+    };
   }, [isMenuOpen]);
 
   return (
